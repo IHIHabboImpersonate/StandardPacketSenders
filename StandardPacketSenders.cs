e03f57a3f5da9945765733088656052ec7efdb1e
Chris Chenery <Cecer1@cecer1.com>
2012-02-01 11:32:55 +0000

    Tidy up file headings.

--- StandardPacketSenders.cs
+++ StandardPacketSenders.cs
@@ -1,11 +1,31 @@
-﻿namespace IHI.Server.Plugins.Cecer1.PacketSenders
+﻿#region GPLv3
+
+// 
+// Copyright (C) 2012  Chris Chenery
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+
+#endregion
+
+namespace IHI.Server.Plugins.Cecer1.PacketSenders
 {
-	[CompatibilityLock(36)]
+    [CompatibilityLock(36)]
     public class PacketSender : Plugin
     {
         public override void Start()
         {
         }
-
     }
 }
