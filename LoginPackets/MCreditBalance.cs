b329e2778c7b28841054cc54bc1c4d49f7fafb1b
Chris Chenery <Cecer1@cecer1.com>
2012-02-01 10:43:56 +0000

    GPLv3

--- LoginPackets/MCreditBalance.cs
+++ LoginPackets/MCreditBalance.cs
@@ -1,23 +1,34 @@
-﻿using IHI.Server.Habbos;
+﻿// 
+// Copyright (C) 2012  Chris Chenery
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using IHI.Server.Habbos;
 
 namespace IHI.Server.Networking.Messages
 {
     public class MCreditBalance : OutgoingMessage
     {
-        public int Balance
-        {
-            get;
-            set;
-        }
-
         /// <summary>
-        /// Constructs a new instance of MCreditBalance and sets the balance to that of a given Habbo.
+        ///   Constructs a new instance of MCreditBalance and sets the balance to that of a given Habbo.
         /// </summary>
-        /// <param name="habbo"></param>
+        /// <param name = "habbo"></param>
         public MCreditBalance(Habbo habbo)
         {
             Balance = habbo.GetCreditBalance();
         }
+
+        public int Balance { get; set; }
 
         public override OutgoingMessage Send(IMessageable target)
         {
--- LoginPackets/MSessionParams.cs
+++ LoginPackets/MSessionParams.cs
@@ -1,112 +1,43 @@
-﻿namespace IHI.Server.Networking.Messages
+﻿// 
+// Copyright (C) 2012  Chris Chenery
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+namespace IHI.Server.Networking.Messages
 {
     public class MSessionParams : OutgoingMessage
     {
-        public int A
-        {
-            get;
-            set;
-        }
-        public int B
-        {
-            get;
-            set;
-        }
-        public int C
-        {
-            get;
-            set;
-        }
-        public int D
-        {
-            get;
-            set;
-        }
-        public string DateFormat
-        {
-            get;
-            set;
-        }
-        public int E
-        {
-            get;
-            set;
-        }
-        public int F
-        {
-            get;
-            set;
-        }
-        public int G
-        {
-            get;
-            set;
-        }
-        public int H
-        {
-            get;
-            set;
-        }
-        public int I
-        {
-            get;
-            set;
-        }
-        public int J
-        {
-            get;
-            set;
-        }
-        public int K
-        {
-            get;
-            set;
-        }
-        public int L
-        {
-            get;
-            set;
-        }
-        public string M
-        {
-            get;
-            set;
-        }
-        public int N
-        {
-            get;
-            set;
-        }
-        public bool O
-        {
-            get;
-            set;
-        }
-        public int P
-        {
-            get;
-            set;
-        }
-        public string Q
-        {
-            get;
-            set;
-        }
-        public int R
-        {
-            get;
-            set;
-        }
-        public bool S
-        {
-            get;
-            set;
-        }
-        public string URL
-        {
-            get;
-            set;
-        }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public int D { get; set; }
+        public string DateFormat { get; set; }
+        public int E { get; set; }
+        public int F { get; set; }
+        public int G { get; set; }
+        public int H { get; set; }
+        public int I { get; set; }
+        public int J { get; set; }
+        public int K { get; set; }
+        public int L { get; set; }
+        public string M { get; set; }
+        public int N { get; set; }
+        public bool O { get; set; }
+        public int P { get; set; }
+        public string Q { get; set; }
+        public int R { get; set; }
+        public bool S { get; set; }
+        public string URL { get; set; }
 
         public override OutgoingMessage Send(IMessageable target)
         {
